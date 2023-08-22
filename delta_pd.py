2ee0bb057123999847974c37aee6e78a8182202a
shadrin24 <shadrin24@bk.ru>
2023-08-22 23:26:25 +0700

    Начало работы над кластером по времени

--- delta_pd.py
+++ delta_pd.py
@@ -2,6 +2,7 @@ import requests
 import datetime
 from dateutil.relativedelta import relativedelta
 import pandas as pd
+import numpy as np
 
 
 def ohlc_delta(link: str, years=0, months=0, days=0, hours=0, minutes=0, seconds=0):
@@ -15,9 +16,22 @@ def ohlc_delta(link: str, years=0, months=0, days=0, hours=0, minutes=0, seconds
     :param seconds: Кол-во секунд
     :return: Файл с данными ohlc с объемами"""
 
-    df = pd.read_json(link)
-    print(df.head())
-    print(df)
+    # Получаем датафрейм с API
+    data_json = pd.read_json(link)
+    data_list = data_json["data"]["data"]
+    data_np = np.array(data_list)
+    df1 = pd.DataFrame(data_np, columns=['date_time', 'cost', 'value'])
+    df1['date_time'] = pd.to_datetime(df1['date_time'])
+    df1['cost'] = pd.to_numeric(df1['cost'])
+    df1['value'] = pd.to_numeric(df1['value'])
+    # ticker = data_json["data"]["ticker"]
+    # df2 = df1['date_time'].str.split(' ', expand=True)
+    # df2.columns = ['date', 'time']
+    # df = pd.concat([df2, df1], axis=1).drop('date_time', axis=1)
+
+    df_cluster = df1.groupby(pd.Grouper(key='date_time', axis=0, freq='2S')).sum()
+    df_cluster = df_cluster.loc[df_cluster['cost'] != 0]
+    print(df_cluster)
 
 
 ohlc_delta("data.json", seconds=2)
